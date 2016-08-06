8b1cde4dff4085eabb0fa050b7d3a3edebe89b00
zeeshan <zeeshan_asyed@hotmail.com>
2016-08-06 15:46:57 +0400

    update

--- conf/index.js
+++ conf/index.js
@@ -3,7 +3,9 @@ module.exports = obj = {
 		EXT:require('../lib/extend'), UTIL:require('../lib/util'),QUERY:require('../lib/query')
 	},
 	BASE : {
-		input:'/input', output:'/Search',rbase:'/gtV1.0', radd : '/add', rget : '/get'
+		input:'/input',rbase:'/gtV1.0', radd : '/add', rget : '/get',
+		message:"/message/", "count":"/count/", "terms":"/terms/", "fstat":"/fieldStat/", "classify":"/fieldFacet/","history":"/history/","cfstat":"/fieldFacetStat/",
+		"fieldSpecificHistory":"/fieldSpecificHistory/","single":"/singleMessage/"
 	},
 	ERRORS : {
 		//[err][relation][subject][object type][this]
